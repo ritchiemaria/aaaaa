3ef1898a82f29c4f45f92700bb88015b7e0512a4
ritchiemaria <ritchiemariahh@gmail.com>
2024-02-22 14:05:57 +0100

    initialisation

---
+++ android/app/src/main/kotlin/com/example/mi_zotra/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mi_zotra
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Stack(
+            children: [
+              Image.asset("", fit: BoxFit.cover,)
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+Container littleBar ({String? texte, IconData? data})
+{
+  Container 
+}
